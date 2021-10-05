764065474214bbb77a10d074901aacf823e119f1
BalaMurugan6 <91958356+BalaMurugan6@users.noreply.github.com>
2021-10-05 18:41:17 +0530

    Add files via upload

---
+++ convert kilometer to miles.py
@@ -0,0 +1,7 @@
+
+
+#to convert kilometer to miles
+kilometer=float(input("enter value in kilometers: "))
+_1km=0.621371
+miles=kilometer*_1km
+print("%0.2f kilometer is equal to %0.2f miles"%(kilometer,miles))
